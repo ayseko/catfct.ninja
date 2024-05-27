ecf9f3b944df62fc34160f67ac262bfe56baf3cf
ayse <ayseko1989@gmail.com>
2024-05-28 00:25:30 +0200

    First commit

--- src/test/java/requests/Get_a_list_of_breeds_limit.java
+++ src/test/java/requests/Get_a_list_of_breeds_limit.java
@@ -3,19 +3,32 @@ package requests;
 import baseURL.CatfctNinja_baseURL;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.junit.Test;
+import pojo.CatfctNinja_pojo;
 
 import static io.restassured.RestAssured.given;
 
-public class Get_a_list_of_breeds extends CatfctNinja_baseURL {
+public class Get_a_list_of_breeds_limit extends CatfctNinja_baseURL {
     @Test
-    public void getBreeds(){
-        specification.pathParam("firstParam","breeds");
-        Response response=given().spec(specification).when().get("/{firstParam}");
-        response.getBody().prettyPrint();
-        response.then().assertThat().statusCode(200);
-        response.then().assertThat().contentType(ContentType.JSON);
+    public void getBreedsWithLimit(){
+        CatfctNinja_pojo pojo = new CatfctNinja_pojo();
+        pojo.setLimit(1);
+        PropertyConfigurator.configure("log4j.properties");
+        Logger logger = Logger.getLogger(Get_a_list_of_breeds_limit.class);
 
+        specification.pathParam("firstParam","breeds")
+                .queryParam("limit",pojo.getLimit());
+        logger.info("User sets the url with parameter and limit");
+        Response response=given().spec(specification).when().get("/{firstParam}");
+        logger.info("User sends the GET request");
+        response.getBody().prettyPrint();
+        logger.info("User displays the breeds list in the response body");
+        response.then().assertThat().statusCode(200);
+        logger.info("User asserts that The status code is 200");
+        response.then().assertThat().contentType(ContentType.JSON);
+        logger.info("User asserts that The content type is application/json");
 
     }
 }
--- src/test/java/requests/Get_facts_max_lenght.java
+++ src/test/java/requests/Get_facts_max_lenght.java
@@ -1,4 +1,38 @@
 package requests;
 
-public class Get_a_list_of_breeds {
+import baseURL.CatfctNinja_baseURL;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.junit.Test;
+import pojo.CatfctNinja_pojo;
+
+import static io.restassured.RestAssured.given;
+
+public class Get_facts_max_lenght extends CatfctNinja_baseURL {
+    @Test
+    public void getFactsMaxLenght(){
+        CatfctNinja_pojo pojo = new CatfctNinja_pojo();
+        pojo.setMax_lenght(70);
+        pojo.setLimit(5);
+
+        PropertyConfigurator.configure("log4j.properties");
+        Logger logger = Logger.getLogger(Get_facts_max_lenght.class);
+
+        specification.pathParam("firstParam","facts")
+                .queryParam("max_length",pojo.getMax_lenght())
+                .queryParam("limit", pojo.getLimit());
+        logger.info("User sets the url with parameter and max_length and limit");
+        Response response=given().spec(specification).when().get("/{firstParam}");
+
+        logger.info("User sends the GET request");
+        response.getBody().prettyPrint();
+        logger.info("User displays the facts list in the response body");
+        response.then().assertThat().statusCode(200);
+        logger.info("User asserts that the status code is 200");
+        response.then().assertThat().contentType(ContentType.JSON);
+        logger.info("User asserts that the content type is application/json");
+
+    }
 }
